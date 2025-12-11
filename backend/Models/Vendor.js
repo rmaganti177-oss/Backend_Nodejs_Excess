dd8e32e983f44a3b005c1294a7df14df90bc28f1
rmaganti177-oss <your-github-email@example.com>
2025-12-11 10:22:45 +0530

    made changes for PORT

---
+++ backend/Models/Vendor.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const VendorSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+   firm: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Firm'
+        }
+    ]
+});
+
+module.exports = mongoose.model("Vendor", VendorSchema);
---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotEnv = require("dotenv");
+const VendorRoutes = require("./Routes/VendorRoutes");
+const FirmRoutes = require("./Routes/FirmRoutes") ;// ADDED
+
+dotEnv.config();
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json()); // REQUIRED
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGOURI)
+.then(() => console.log("MongoDB Connected Successfully"))
+.catch(err => console.log("MongoDB Connection Error:", err));
+
+app.use('/Vendor', VendorRoutes);
+app.use('/Firm', FirmRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
